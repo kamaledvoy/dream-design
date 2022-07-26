3a934ed267eafbdddacbb60afb0d5628591fe301
kamalEdvoy <kamal@edvoy.com>
2022-07-27 01:59:53 +0530

    service page added

--- elmechenterprises/assets/js/app.js
+++ elmechenterprises/assets/js/app.js
@@ -38,6 +38,7 @@ window.addEventListener("scroll", function () {
   var scroll = document.querySelector(".scrollTop");
   scroll.classList.toggle("active", window.scrollY > 500);
 });
+
 function scrollToTop() {
   window.scroll({
     top: 0,
@@ -241,67 +242,6 @@ $(document).ready(function () {
     },
   });
 });
-
-// hero slider
-/*const $slides = $(".owl-carousel .owl-slide");
-$slides.css("height", $(window).height());
-$(window).resize(() => {
-  $slides.css("height", $(window).height());
-});*/
-
-$(".mainslider").on("initialized.owl.carousel", () => {
-  setTimeout(() => {
-    $(".owl-item.active .owl-slide-animated").addClass("is-transitioned");
-    $("section").show();
-  }, 200);
-});
-
-const $owlCarousel = $(".mainslider").owlCarousel({
-  items: 1,
-  loop: true,
-  nav: true,
-  navText: [
-    '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
-    '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>' /* icons from https://iconmonstr.com */,
-  ],
-});
-
-$owlCarousel.on("changed.owl.carousel", (e) => {
-  $(".owl-slide-animated").removeClass("is-transitioned");
-
-  const $currentOwlItem = $(".owl-item").eq(e.item.index);
-  $currentOwlItem.find(".owl-slide-animated").addClass("is-transitioned");
-
-  const $target = $currentOwlItem.find(".owl-slide-text");
-  doDotsCalculations($target);
-});
-
-$owlCarousel.on("resize.owl.carousel", () => {
-  setTimeout(() => {
-    setOwlDotsPosition();
-  }, 50);
-});
-
-/*if there isn't content underneath the carousel*/
-//$owlCarousel.trigger("refresh.owl.carousel");
-
-setOwlDotsPosition();
-
-function setOwlDotsPosition() {
-  const $target = $(".owl-item.active .owl-slide-text");
-  doDotsCalculations($target);
-}
-
-function doDotsCalculations(el) {
-  const height = el.height();
-  const { top, left } = el.position();
-  const res = height + top + 20;
-
-  $(".owl-carousel .owl-dots").css({
-    top: `${res}px`,
-    left: `${left}px`,
-  });
-}
 
 //wow
 wow = new WOW({
